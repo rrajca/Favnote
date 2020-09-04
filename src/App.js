eec987c31d1cd16c296bf33061a5ff00b28b526f
Remigiusz Rajca <remigiusz.rajca@gmail.com>
2020-09-04 12:52:13 +0200

    init create-react-app project with installed eslint & prettier

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. A quam modi aliquam nobis illo
+          aliquid laboriosam libero, quaerat perferendis, quia eaque vitae fuga mollitia vel? Ex
+          ipsum minus facere rem?
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
