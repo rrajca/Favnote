ec606b54ca301272a7c2f0574818be1975969af1
Remigiusz Rajca <remigiusz.rajca@gmail.com>
2020-09-07 09:27:29 +0200

    add new component Heading

--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -7,4 +7,8 @@ export const theme = {
   black: 'hsl(0, 0%, 0%)',
   light: 300,
   bold: 600,
+  fontSize: {
+    l: '2.4rem',
+    xl: '4rem',
+  },
 };
--- src/views/root/Root.js
+++ src/views/root/Root.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import Button from 'components/atoms/Button/Button';
 import GlobalStyle from 'theme/GlobalStyle';
 import { theme } from 'theme/mainTheme';
+
+import Button from 'components/atoms/Button/Button';
+import Heading from 'components/atoms/Heading/Heading';
 
 function Root() {
   return (
@@ -14,6 +16,8 @@ function Root() {
           <h1>Hello</h1>
           <Button>Close / Save</Button>
           <Button secondary>Remove</Button>
+          <Heading big>Notes</Heading>
+          <Heading>Small header</Heading>
         </>
       </ThemeProvider>
     </div>
