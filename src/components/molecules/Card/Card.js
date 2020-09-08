5dfef36f85f40ed28d7a48e59d0d11b89eab7bc9
Remigiusz Rajca <remigiusz.rajca@gmail.com>
2020-09-08 13:23:38 +0200

    add new molecule Card

---
+++ src/components/molecules/Card/Card.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Heading from 'components/atoms/Heading/Heading';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Button from 'components/atoms/Button/Button';
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 385px;
+  border-radius: 10px;
+  box-shadow: 0 5px 30px -10px hsla(0, 0%, 0%, 0.1);
+  overflow: hidden;
+`;
+
+const InnerWrapper = styled.div`
+  padding: 15px 30px;
+  background-color: ${({ theme, yellow }) => (yellow ? theme.primary : 'white')};
+
+  ${({ flex }) =>
+    flex &&
+    css`
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: baseline;
+      flex: 1;
+    `}
+`;
+
+const StyledHeading = styled(Heading)`
+  margin: 0 0 5px 0;
+`;
+
+const DateInfo = styled(Paragraph)`
+  margin: 0;
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+`;
+
+const Card = () => (
+  <StyledWrapper>
+    <InnerWrapper yellow>
+      <StyledHeading>Hello Roman</StyledHeading>
+      <DateInfo>03.07.2020</DateInfo>
+    </InnerWrapper>
+    <InnerWrapper flex>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia saepe quas, enim minus
+        ducimus animi perspiciatis suscipit cum modi repellendus obcaecati atque maiores,
+        voluptatibus dolore! Commodi quis perspiciatis tenetur fugiat?
+      </Paragraph>
+      <Button secondary>Remove</Button>
+    </InnerWrapper>
+  </StyledWrapper>
+);
+
+export default Card;
---
+++ src/components/molecules/Card/Card.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from 'components/molecules/Card/Card';
+
+export default {
+  title: 'Card',
+  component: Card,
+};
+
+export const card = () => {
+  return <Card />;
+};
